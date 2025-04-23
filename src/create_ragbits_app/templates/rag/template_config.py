f70a1d5ecc8e6fe2734ca4b33deff7d0c6dc7054
Mateusz Hordyński <mateusz.hordynski@deepsense.ai>
2025-04-23 19:26:09 +0200

    add new config values

--- src/create_ragbits_app/templates/rag/template_config.py
+++ src/create_ragbits_app/templates/rag/template_config.py
@@ -3,6 +3,7 @@ Configuration for the RAG template.
 """
 from create_ragbits_app.template_config_base import (
     TemplateConfig, 
+    ListQuestion,
 )
 from typing import List
 
@@ -12,7 +13,24 @@ class RagTemplateConfig(TemplateConfig):
     name: str = "RAG (Retrieval Augmented Generation)"
     description: str = "Basic RAG (Retrieval Augmented Generation) application"
     
-    questions: List = []
+    questions: List = [
+        ListQuestion(
+            name="vector_db", 
+            message="What Vector database you want to use?",
+            choices=[
+                "Qdrant",
+                "Postgresql with pgvector",
+            ]
+        ),
+        ListQuestion(
+            name="parser", 
+            message="What parser you want to use parse documents?",
+            choices=[
+                "unstructured",
+                "docling",
+            ]
+        ),
+    ]
 
 # Create instance of the config to be imported
 config = RagTemplateConfig()
